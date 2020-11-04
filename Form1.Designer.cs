e34a28ed7c8d3aeca4818261ccf384b67e6e2b42
karnati8 <janson.pieter@gmail.com>
2020-11-04 08:59:36 +0200

    Cannot get Youtube url's to work
    Cannot get Youtube url's to work. Sharing this to VLC forums to debug.

---
+++ Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace TestLibVLCSharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblURL = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.txtURL = new System.Windows.Forms.TextBox();
+            this.cboResolution = new System.Windows.Forms.ComboBox();
+            this.chkYouTube = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // lblURL
+            // 
+            this.lblURL.AutoSize = true;
+            this.lblURL.Location = new System.Drawing.Point(53, 48);
+            this.lblURL.Name = "lblURL";
+            this.lblURL.Size = new System.Drawing.Size(40, 17);
+            this.lblURL.TabIndex = 0;
+            this.lblURL.Text = "URL:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(53, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(143, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Preferred Resolution:";
+            // 
+            // btnGo
+            // 
+            this.btnGo.Enabled = false;
+            this.btnGo.Location = new System.Drawing.Point(56, 160);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(75, 23);
+            this.btnGo.TabIndex = 2;
+            this.btnGo.Text = "Go";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // txtURL
+            // 
+            this.txtURL.Location = new System.Drawing.Point(108, 45);
+            this.txtURL.Name = "txtURL";
+            this.txtURL.Size = new System.Drawing.Size(665, 22);
+            this.txtURL.TabIndex = 3;
+            this.txtURL.Text = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+            this.txtURL.TextChanged += new System.EventHandler(this.txtURL_TextChanged);
+            // 
+            // cboResolution
+            // 
+            this.cboResolution.FormattingEnabled = true;
+            this.cboResolution.Items.AddRange(new object[] {
+            "-1, Best available",
+            "1080, Full HD (1080p)",
+            "720, HD (720p)",
+            "576, Standard Definition (576 or 480 lines)",
+            "360, Low Definition (360 lines)",
+            "240, (Very Low Definition (240 lines)"});
+            this.cboResolution.Location = new System.Drawing.Point(202, 98);
+            this.cboResolution.Name = "cboResolution";
+            this.cboResolution.Size = new System.Drawing.Size(295, 24);
+            this.cboResolution.TabIndex = 4;
+            this.cboResolution.SelectedIndexChanged += new System.EventHandler(this.cboResolution_SelectedIndexChanged);
+            // 
+            // chkYouTube
+            // 
+            this.chkYouTube.AutoSize = true;
+            this.chkYouTube.Checked = true;
+            this.chkYouTube.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkYouTube.Location = new System.Drawing.Point(675, 101);
+            this.chkYouTube.Name = "chkYouTube";
+            this.chkYouTube.Size = new System.Drawing.Size(102, 21);
+            this.chkYouTube.TabIndex = 8;
+            this.chkYouTube.Text = "Is YouTube";
+            this.chkYouTube.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1280, 768);
+            this.Controls.Add(this.chkYouTube);
+            this.Controls.Add(this.cboResolution);
+            this.Controls.Add(this.txtURL);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblURL);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private LibVLCSharp.WinForms.VideoView videoView1;
+        private System.Windows.Forms.Label lblURL;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.TextBox txtURL;
+        private System.Windows.Forms.ComboBox cboResolution;
+        private System.Windows.Forms.CheckBox chkYouTube;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,122 @@
+﻿using LibVLCSharp.Shared;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TestLibVLCSharp
+{
+    public partial class Form1 : Form
+    {
+        public LibVLCSharp.WinForms.VideoView videoView2;
+
+        public Form1()
+        {
+            if (!DesignMode)
+            {
+                Core.Initialize();
+            }
+
+            InitializeComponent();
+
+            videoView2 = new LibVLCSharp.WinForms.VideoView();
+
+            this.SuspendLayout();
+             
+            videoView2.BackColor = System.Drawing.Color.Black;
+            videoView2.Location = new System.Drawing.Point(0, 0);
+            videoView2.MediaPlayer = null;
+            videoView2.Name = "videoView2";
+            videoView2.Size = new System.Drawing.Size(1280, 768);
+            videoView2.TabIndex = 0;
+            videoView2.Text = "videoView2";
+            
+            videoView2.Visible = false;
+
+            this.Controls.Add(videoView2);
+            videoView2.Click += VideoView2_Click;
+            this.ResumeLayout();
+            
+        }
+
+        private void _mp_EncounteredError(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void VideoView2_Click(object sender, EventArgs e)
+        {           
+
+            videoView2.Visible = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void btnGo_Click(object sender, EventArgs e)
+        {
+
+            if (chkYouTube.Checked)
+            {
+                using (LibVLC _libVLC = new LibVLC("--preferred-resolution=" + cboResolution.SelectedItem.ToString().Split(',')[0]))
+                {
+                    Media media = new Media(_libVLC, txtURL.Text.Trim(), FromType.FromLocation);
+                    await media.Parse(MediaParseOptions.ParseNetwork);
+
+                    using (MediaPlayer _mp = new MediaPlayer(media.SubItems.First()))
+                    {
+                        videoView2.MediaPlayer = _mp;
+                        videoView2.Visible = true;
+                        videoView2.BringToFront();
+                        var r = _mp.Play();
+                    }
+                }
+                    
+                
+            }
+            else
+            {
+                LibVLC _libVLC = new LibVLC("--preferred-resolution=" + cboResolution.SelectedItem.ToString().Split(',')[0]);
+
+                Media media = new Media(_libVLC, txtURL.Text.Trim(), FromType.FromLocation);
+                
+                MediaPlayer _mp = new MediaPlayer(_libVLC);
+                
+                _mp.EncounteredError += _mp_EncounteredError;
+                videoView2.MediaPlayer = _mp;
+                videoView2.Visible = true;
+                videoView2.BringToFront();
+                _mp.Play(media);
+            }
+                
+        }
+
+        private void cboResolution_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            EnableGoButton();
+        }
+
+        private void EnableGoButton()
+        {
+            if (cboResolution.SelectedIndex >= 0 && txtURL.Text.Trim() != String.Empty)
+            {
+                btnGo.Enabled = true;
+            }
+            else
+            {
+                btnGo.Enabled = false;
+            }
+        }
+
+        private void txtURL_TextChanged(object sender, EventArgs e)
+        {
+            EnableGoButton();
+        }
+    }
+}
